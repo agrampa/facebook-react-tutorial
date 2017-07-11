eb890459bf74273de823da4c5f7df41d365cb9fd
Allie Grampa <agrampa@yahoo.com>
2017-07-10 17:57:45 -0700

    final code for tutorial

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -59,12 +59,13 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
-      xIsNext: true;
+      stepNumber: 0,
+      xIsNext: true,
     }
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = this.state.squares.slice();
     if(calculateWinner(squares) || squares[i]) {
@@ -75,13 +76,21 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
@@ -89,7 +98,7 @@ class Game extends React.Component {
         'Move #' + move :
         'Game Start';
       return (
-        <li>
+        <li key={move}>
           <a href="#" onClick={() => this.jumpTo(move)}>desc</a>
         </li>
       );
